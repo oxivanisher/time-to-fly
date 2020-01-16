f1dd6f9f5e749721be883239ad2b492be1a751fa
Daniel Steuri <daniel.steuri@flowingones.ch>
2020-01-16 21:40:31 +0100

    Add correct namespace

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -2,5 +2,5 @@
 
 defined('C5_EXECUTE') or die('Access Denied.');
 
-Route::register('/api/timetofly/get/simple', '\Application\Controller\CalculateTime::getSimple');
-Route::register('/api/timetofly/get/list', '\Application\Controller\CalculateTime::getList');
+Route::register('/api/timetofly/get/simple', '\Concrete\Package\TimeToFly\Controller\CalculateTime::getSimple');
+Route::register('/api/timetofly/get/list', '\Concrete\Package\TimeToFly\Controller\CalculateTime::getList');
